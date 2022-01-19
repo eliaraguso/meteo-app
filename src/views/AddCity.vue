1d6db85ed44a1bd6c355facb44a1f571c2b58973
eliaraguso <elia.raguso@gmail.com>
2022-01-19 12:38:48 +0100

    aggiunta struttura base + stile a pagina AddCity.vue

--- src/views/AddCity.vue
+++ src/views/AddCity.vue
@@ -19,3 +19,16 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .grid {
+    display: grid;
+    padding-top: 81px;
+    background-color: #31363d;
+    width: 100%;
+    min-height: 100vh;
+    @media (min-width: 400px){
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
