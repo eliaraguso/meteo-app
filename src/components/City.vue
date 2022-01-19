ad0d0b2f97febfa0a67b79bf246166aa08c5f5ae
eliaraguso <elia.raguso@gmail.com>
2022-01-19 14:47:41 +0100

    aggiunta icona + video dinamici (corrispondenti alle reali condizioni meteo) nel componente City,

--- src/components/City.vue
+++ src/components/City.vue
@@ -1,10 +1,23 @@
 <template>
-  <h4>City component</h4>
+  <div class="city">
+      <span>{{this.city.city}}</span>
+      <div class="weather">
+          <span>{{Math.round(this.city.currentWeather.main.temp)}}&deg;</span>
+          <img :src="require(`../../public/conditions/${this.city.currentWeather.weather[0].icon}.png`)" alt="">
+      </div>
+      <div class="video">
+          <video autoplay loop muted :src="require(`../../public/videos/${this.city.currentWeather.weather[0].icon}.mp4`)"></video>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
     name: "City",
+    props: ['city'],
+    created() {
+        console.log(this.city);
+    },
 }
 </script>
 
